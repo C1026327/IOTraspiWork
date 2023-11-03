9d59a85cc15cd58f5d05a2d72355f95c34ffe89d
C1026327 <118294547+C1026327@users.noreply.github.com>
2023-11-03 13:45:23 +0000

    fixed it :)

--- rpi_codes/programming.py
+++ rpi_codes/programming.py
@@ -16,12 +16,12 @@ class Device(Base):
     version = Column(Integer, nullable = False)
     lastUpdated = Column(DateTime, nullable = False)
 
-def __init__(self, deviceID, tag, status, version, lastUpdated):
-    self.deviceID=deviceID
-    self.tag=tag
-    self.status=status
-    self.version=version
-    self.lastUpdated=lastUpdated
+    def __init__(self, deviceID, tag, status, version, lastUpdated):
+        self.deviceID=deviceID
+        self.tag=tag
+        self.status=status
+        self.version=version
+        self.lastUpdated=lastUpdated
 
 class StreamingData(Base):
     __tablename__ = 'streaming_data'
@@ -30,53 +30,53 @@ class StreamingData(Base):
     sensingTime = Column(DateTime, nullable=False)
     timestamp = Column(DateTime, default=func.now())
 
-def __init__(self,id,deviceId,data,sensingTime,timestamp):
-    self.id = id
-    self.data=data
-    self.deviceId=deviceId
-    self.sensingTime=sensingTime
-    self.timestamp=timestamp
+    def __init__(self,id,deviceId,data,sensingTime,timestamp):
+        self.id = id
+        self.data=data
+        self.deviceId=deviceId
+        self.sensingTime=sensingTime
+        self.timestamp=timestamp
 
-def reg_device(self,device,engine):
-    Session = sessionmaker(engine)
-    with Session() as session:
-        session.add(device)
-        session.commit()
+    def reg_device(self,device,engine):
+        Session = sessionmaker(engine)
+        with Session() as session:
+            session.add(device)
+            session.commit()
 
-def add_data(self,data_metadata,engine):
-    Session=sessionmaker(engine)
-    with Session() as session:
-        session.add(data_metadata)
-        session.commit()
+    def add_data(self,data_metadata,engine):
+        Session=sessionmaker(engine)
+        with Session() as session:
+            session.add(data_metadata)
+            session.commit()
 
-def main():
-    functions_names = [register_device_menu]
-    #, device_to_sense_menu, show_all_sensed_data, exit_menu]
-    menu_items = dict(enumerate(functions_names, start=1))
-    while True:
-        show_menu(menu_items)
-        selection = int(
-            input("Please enter your desired function number: "))
-        selected_value = menu_items[selection]
-        selected_value()
+    def main():
+        functions_names = [register_device_menu]
+        #, device_to_sense_menu, show_all_sensed_data, exit_menu]
+        menu_items = dict(enumerate(functions_names, start=1))
+        while True:
+            show_menu(menu_items)
+            selection = int(
+                input("Please enter your desired function number: "))
+            selected_value = menu_items[selection]
+            selected_value()
 
-def show_menu(menu):
-    for i, function in menu.items():
-        print(i,function.__name__)
+    def show_menu(menu):
+        for i, function in menu.items():
+            print(i,function.__name__)
 
-def register_device_menu():
-    print("\n This is the device registration menu.")
-    deviceTag=input("Please enter the tag for a device: ")
-    status = input("Please enter the status of the device: ")
-    version = input("Please enter the device version: ")
-    device = Device(None,deviceTag,status,version,dt.now())
-    device.reg_device(device,engine)
-    print ("Your device was registered successfully.\n")
+    def register_device_menu():
+        print("\n This is the device registration menu.")
+        deviceTag=input("Please enter the tag for a device: ")
+        status = input("Please enter the status of the device: ")
+        version = input("Please enter the device version: ")
+        device = Device(None,deviceTag,status,version,dt.now())
+        device.reg_device(device,engine)
+        print ("Your device was registered successfully.\n")
 
-if __name__ == "__main__":
-    engine = create_engine('sqlite:///iot.db', echo=True)
-    Base.metadata.create_all(engine)
-    main()
+    if __name__ == "__main__":
+        engine = create_engine('sqlite:///iot.db', echo=True)
+        Base.metadata.create_all(engine)
+        main()
 #     deviceTag=''.join((random.choice('environment') for i in range(5)))
 #     status="enabled"
 #     version="1.0"
